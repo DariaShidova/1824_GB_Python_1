848f6f0f84f55d5e4f1f04306bc79a9cc35edf4a
Dashenka <dariamix@inbox.ru>
2021-10-30 18:48:49 +0300

    sp

--- mikhailova_daria_dz_7/task_7_3.py
+++ mikhailova_daria_dz_7/task_7_3.py
@@ -1,18 +1,22 @@
 import os
 import shutil
-#Я ТОЛЬКО СЕЙЧАС ПОНЯЛА ЧТО СДЕЛАЛА СОВСЕМ НЕ ТООООО
-#сама придумала себе задание, подумала что надо просто папки которые внутри переложить в новую
-#НАДЕЮСЬ УСПЕЮ ИСПРАВИТЬ ДО ТОГО КАК ВЫ ПРОВЕРИТЕ :((((
+#не уверенна что нужно было сделать именно это.
+
 folder = 'templates'
 main_folder = 'my_project'
-creation = (os.path.join(main_folder, folder))
+format_file = '.html'
 
-for root, dirs, files in os.walk(main_folder):
-    if not os.path.isdir(creation):
-        os.makedirs(creation)
+creation = (os.path.join(main_folder, folder))
+if not os.path.isdir(creation):
+    os.makedirs(creation)
+
+for root, dirs, files in os.walk((os.path.normpath(main_folder)), topdown=False):
     try:
-        for d in dirs:
-            if d != folder:
-                new_location = shutil.move(f'{main_folder}/{d}', f'{creation}/{d}')
-    except FileNotFoundError as e:
-        print(f'error: {e}')
+        for file in files:
+            if file.endswith(format_file):
+                print(root)
+                folder_in_file = root.split('\\')[-1]
+                new_location = os.path.join(creation, folder_in_file)
+                shutil.copytree(root, new_location)
+    except FileExistsError as e:
+        break
