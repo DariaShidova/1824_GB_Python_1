901e7689753beec12b95584bde344f54a2660e57
Dashenka <dariamix@inbox.ru>
2021-11-04 23:04:38 +0300

    1

--- mikhailova_daria_dz_9/task_9_5.py
+++ mikhailova_daria_dz_9/task_9_5.py
@@ -8,22 +8,22 @@ class Stationery:
 
 class Pen(Stationery):
     def draw(self):
-        return super().draw(), f'{self.title}'
+        return f'{super().draw()}: {self.title}'
 
 
 class Pencil(Stationery):
     def draw(self):
-        return super().draw(), f'{self.title}'
+        return f'{super().draw()}: {self.title}'
 
 
 class Handle(Stationery):
     def draw(self):
-        return super().draw(), f'{self.title}'
+        return f'{super().draw()}: {self.title}'
 
 
 pen = Pen('pen')
 pencil = Pencil('pencil')
 handle = Handle('handle')
-print(*pen.draw())
-print(*pencil.draw())
-print(*handle.draw())
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
