0b055646e3f541613fb6a176f6e1a3e716d73fd1
Dashenka <dariamix@inbox.ru>
2021-11-11 14:50:57 +0300

    1

--- mikhailova_daria_dz_10/task_10_2.py
+++ mikhailova_daria_dz_10/task_10_2.py
@@ -1,1 +1,30 @@
-#надеюсь успею докамитить до того как проверите
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+
+    def __init__(self, parameter):
+        self.parameter = parameter
+
+    @property
+    def general(self):
+        return f'Сумма : {self.parameter / 6.5 + 0.5 + 2 * self.parameter + 0.3 :.2f}'
+
+
+class Coat(Clothes):
+    def consumption(self):
+        return f'Для пальто: {self.parameter / 6.5 + 0.5 :.2f} ткани'
+
+
+class Costume(Clothes):
+    def consumption(self):
+        return f'Для костюма: {2 * self.parameter + 0.3 :.2f} ткани'
+
+
+a = Coat(10)
+b = Costume(2)
+print(a.consumption())
+print(b.consumption())
+
+
+
